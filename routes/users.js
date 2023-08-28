896ceb08fc0ccc67b95f2348f37213797209efcb
ralph <ralphdcouto1987@gmail.com>
2023-08-28 13:33:56 -0400

    Created home page and post login page

--- routes/users.js
+++ routes/users.js
@@ -7,9 +7,38 @@
 
 const express = require('express');
 const router  = express.Router();
+const path = require('path');
 
-router.get('/', (req, res) => {
-  res.render('users');
-}); 
+const user = true;
+
+router.get("/", (req, res) => {
+  
+  if (user) {
+    // If user is logged in, show the post-login page
+    res.redirect("/users/post_login");
+  } else {
+    // If user is not logged in, show the main page or login page
+    res.redirect("/");
+  }
+});
+
+router.get("/post_login", (req, res) => {
+  res.send("This is the post login page.");
+});
+
+
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  // Sample logic (use your actual logic for authentication here)
+  if (isValidUser(username, password)) {
+    // assuming user authentication is successful
+    // Ideally, you would also set a session or token here to keep the user logged in
+    res.redirect("/post_login");
+  } else {
+    res.send("Invalid login credentials");
+  }
+})
+
 
 module.exports = router;
