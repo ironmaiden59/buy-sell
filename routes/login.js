92204338319a54fa7d51faf6d562badad8f36b6d
ralph <ralphdcouto1987@gmail.com>
2023-08-29 16:59:28 -0400

    moved the post new car to header

--- routes/login.js
+++ routes/login.js
@@ -15,7 +15,8 @@ router.post('/', (req, res) => {
   res.cookie('username', email);
 
   // Redirect to the home page
-  res.redirect('/dashboard');
+  res.redirect('/');
 });
 
 module.exports = router;
+
